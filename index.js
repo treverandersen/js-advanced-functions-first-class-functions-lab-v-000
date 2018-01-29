5950d02489abcccaafa0f1f9a0af0172b8f08819
Trever Andersen <trever.r.andersen@gmail.com>
2018-01-29 19:27:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ function returnFirstTwoDrivers(arr) {
   const firstTwo = function (arr) {
     return arr.slice(0, 2);
   };
-  //return firstTwo(arr);
+  return firstTwo(arr);
+
   const firstTwo = function () {
     returnFirstTwoDrivers = 'testing reassignment';
   };
+
 };

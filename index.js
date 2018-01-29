fe4f08c0e73776ef46585a4a76877ef002cca3ac
Trever Andersen <trever.r.andersen@gmail.com>
2018-01-29 18:06:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file!
 function returnFirstTwoDrivers() {
   const firstTwo = function (arr) {
-    return arr.slice(0, 2).join( )
+    return arr.slice(0, 2).join( );
   };
 };
